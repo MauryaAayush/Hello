c88656c6848878f732fcd0d7c4b1b5dca81bf594
Aayush <maayush235@gmail.com>
2023-10-04 23:08:00 +0530

    a

--- h3.java
+++ h3.java
@@ -2,5 +2,7 @@ public class h3 {
     public static void main(String[] agrs)
     {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("Hello world");
     }
 }
