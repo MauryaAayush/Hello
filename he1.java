94e75f1637be097338c55c4252536285db6f3483
Aayush <maayush235@gmail.com>
2023-10-06 00:18:38 +0530

    A

--- he1.java
+++ he1.java
@@ -2,5 +2,8 @@ public class he1 {
     public static void main(String[] args)
     {
         System.out.println("hello everyone");
+        System.out.println("hello everyone");
+        System.out.println("hello everyone");
+        System.out.println("hello everyone");
     }
 }
