b4cba57e59a7d13b21a2579560c4231a8a81d83a
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2023-11-10 20:56:01 +0530

    Update h3.java

--- h3.java
+++ h3.java
@@ -7,14 +7,14 @@ public class h3 {
         System.out.println("hello world");
         System.out.println("Hello world");
          System.out.println("Hello world");
- System.out.println("Hello world");
+     System.out.println("Hello world");
          System.out.println("Hello world");
- System.out.println("Hello world");
+     System.out.println("Hello world");
          System.out.println("Hello world");
- System.out.println("Hello world");
+     System.out.println("Hello world");
          System.out.println("Hello world");
- System.out.println("Hello world");
+     System.out.println("Hello world");
          System.out.println("Hello world");
- System.out.println("Hello world");
+     System.out.println("Hello world");
     }
 }
