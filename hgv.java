3bfcdcf986eb7f1b7f63b1649361a9af8eb63a5f
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2023-11-20 19:11:49 +0530

    Update hgv.java

--- hgv.java
+++ hgv.java
@@ -6,7 +6,7 @@ public class hgv {
         System.out.println("hello world");
         System.out.println("Hello world");
          System.out.println("hello world");
-        System.out.println("Hello world");
+
     }
 }
 
