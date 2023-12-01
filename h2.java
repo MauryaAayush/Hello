8d4bb2fa4f41ab21c942208043bd2530175de461
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2023-12-01 10:22:17 +0530

    Update h2.java

--- h2.java
+++ h2.java
@@ -10,9 +10,6 @@ public class h2 {
         System.out.println("Hello world");
         System.out.println("hello world");   
         System.out.println("Hello world");
-        System.out.println("hello world");
-        System.out.println("Hello world");
-        System.out.println("hello world");
-        S
+      
     }
 }
