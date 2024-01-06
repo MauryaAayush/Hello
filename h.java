4feeeb3885804170bd68476346d504c4e6746119
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2024-01-06 15:57:15 +0530

    Update h.java

--- h.java
+++ h.java
@@ -8,9 +8,6 @@ public static void main(String[] args)
   System.out.println("hello world");
   System.out.println("Hello world");
    System.out.println("Hello world");
-   System.out.println("Hello world");
-
-  
-
+   System.out.println("Hello wor
 }
 }
