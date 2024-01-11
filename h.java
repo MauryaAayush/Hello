64cd28b6730eaeb56e81d831f985cefbf7d5d55a
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2024-01-11 12:51:23 +0530

    Update h.java

--- h.java
+++ h.java
@@ -6,7 +6,6 @@ public static void main(String[] args)
   System.out.println("hello world");
   System.out.println("hello world");
   System.out.println("hello world");
-  System.out.println("Hello world");
-   
-}
+  System.out.println("Hello world"); 
+  }
 }
