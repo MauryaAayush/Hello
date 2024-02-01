86efdef7053cbdeb93fb9d6a8b89b9506ab7fbf8
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2024-02-01 17:37:58 +0530

    Update H.java

--- H.java
+++ H.java
@@ -5,8 +5,7 @@ public class H {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        
         
     }
 }
